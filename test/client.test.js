cb3b56fa2a1bc9c92a3fa8959634a831b3b9c10a
zhao wen qiang <13143410521@163.com>
2018-12-03 02:12:57 +0800

    client unbind port

--- test/client.test.js
+++ test/client.test.js
@@ -1,13 +1,18 @@
 const Client = require('../index').client
 const options = {
     project: 'test-client',
-    port: 9528,
+    port: '',
     server_port: 9527,
     server_host: '127.0.0.1'
 }
 const logger = new Client(options)
+const logger2 =new Client(options)
+console.log(logger === logger2)
 describe('cleint', () => {
     it('info', async () => {
+        for (let i = 0; i < 10; i++) {
+            await logger2.info('你好啊')
+        }
         for (let i = 0; i < 10; i++) {
             await logger.info('你好啊')
         }
